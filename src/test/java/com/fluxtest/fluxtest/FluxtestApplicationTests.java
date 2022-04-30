c14c9052cd638d639d11b97751032fe78cd56fe0
kimsangjae <zzai_sang@naver.com>
2022-04-30 14:20:30 +0900

    init

---
+++ src/test/java/com/fluxtest/fluxtest/FluxtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxtest.fluxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
